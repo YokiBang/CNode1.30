c44e05dd39159e4cf294a10fdf227b2831ce7757
lijiaqi <ljqedit@163.com>
2019-02-13 18:19:29 +0800

    123
    123

--- CNode社区/pages/share/share.js
+++ CNode社区/pages/share/share.js
@@ -1,6 +1,8 @@
-// pages/add/add.js
+//获取应用实例
+const app = getApp()
+//引入wxParse
+var WxParse = require('../wxParse/wxParse.js');
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -25,7 +27,32 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    })
+    var that = this;
+    console.log(options)
+    that.setData({
+      activityId: options.activityId
+    })
 
+    wx.request({
+      url: app.globalData.subDomain + '/GetActivityInfo',
+      data: {
+        token: app.globalData.token,
+        activity: that.data.activityId
+      },
+      method: 'POST',
+      success: function (res) {
+        //获取html代码      
+        that.setData({
+          article: unescape(res.data.activity.aintroduct)
+        })
+        WxParse.wxParse('article', 'html', that.data.article, that, 5);
+        wx.hideLoading();
+      }
+    })
   },
 
   /**
