f76493bd20ef51b8bf1851c667029c66546f45e7
lijiaqi <ljqedit@163.com>
2019-02-14 12:01:12 +0800

    123
    123

---
+++ WX.CNode/WX.CNode.Repository/CollectRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WX.CNode.Repository 
+{
+    using WX.CNode.IRepository;
+    public class CollectRepository : ICollectRepository
+    {
+        public List<Action> CollectAuthorid(int Authorid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
