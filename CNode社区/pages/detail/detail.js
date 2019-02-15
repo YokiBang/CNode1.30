6bfe72f46065477f91ea3196fb534b2096d1613a
zxtwofat <46983800+zxtwofat@users.noreply.github.com>
2019-02-15 14:52:13 +0800

    1

--- CNode社区/pages/detail/detail.js
+++ CNode社区/pages/detail/detail.js
@@ -57,6 +57,8 @@ Page({
     Api.fetchGet(ApiUrl, (err, res) => {
       if(res){
           var detail = that.data.detail;
+
+
           detail.is_collect = true;
           that.setData({
           collectText: "取消收藏",
